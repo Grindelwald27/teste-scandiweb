288a47592e3d2af8e637f9750ea901e246af3211
Caio Oliveira <ccaio.oliveira83@gmail.com>
2022-03-01 19:02:08 -0300

    concluindo o teste

--- js/ind.js
+++ js/ind.js
@@ -4,19 +4,12 @@ let input = document.querySelectorAll('.delete-checkbox');
 
 let divElement = document.querySelectorAll('.col-2');
 
-let principal = document.querySelectorAll('.itens');
+let principal = document.querySelector('.itens');
 
-btn.onclick = function(){	
+btn.onclick = function(){
 	for (let i = 0; i < input.length; i++){
-		for (let p = 0; p < 3; p++){
-			i = 0;
-			p = 0;
-			if (principal[p]){
-				if (input[i].checked){
-					principal.splice(divElement[i], 1);
-				}
-			}
+		if (input[i].checked){
+			input[i].parentElement.remove();
 		}
-		
-	}
+	}	
 }
