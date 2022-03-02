ce9ff5eb811edd227273c24640094f7876c1c001
Caio Oliveira <ccaio.oliveira83@gmail.com>
2022-03-02 15:00:31 -0300

    alterando arquivo ind.js

--- js/ind.js
+++ js/ind.js
@@ -2,10 +2,6 @@ let btn = document.querySelector('#delete-product-btn');
 
 let input = document.querySelectorAll('.delete-checkbox');
 
-let divElement = document.querySelectorAll('.col-2');
-
-let principal = document.querySelector('.itens');
-
 btn.onclick = function(){
 	for (let i = 0; i < input.length; i++){
 		if (input[i].checked){
